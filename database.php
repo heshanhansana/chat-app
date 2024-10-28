c2e80c1fec537f1b6e90b07a7ef851ea0113bf2a
Heshan Hansana <65733812+heshanhansana@users.noreply.github.com>
2024-10-28 08:23:33 +0530

    Basic input field

---
+++ database.php
@@ -0,0 +1,15 @@
+
+<?php
+
+$host = "localhost";
+$db_name = "chat_app";
+$db_username = "root"; 
+$db_password = "";
+
+$conn = new mysqli($host, $db_username, $db_password, $db_name);
+
+if($conn->connect_error){
+    die("Connection Failed". $conn->connect_error);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+    include('database.php');
+
+?>
+
+
+
+<html>
+    <body>
+
+        <div class="container" align="center">
+
+            <div class="headder">
+                <h1>Chat-App</h1>
+                <p>Version-01</p>
+            </div>
+
+
+            <!-- Message display area -->
+            <div class="body">
+
+            </div>
+
+
+            <!-- user input display area -->
+            <div class="user_input">
+
+                <form action="" method="post">
+                    <lable>New message : </lable>
+                    <input type="text" name="msg">
+                    <input type="submit" value="Send">
+                </form>
+
+            </div>
+
+        </div>
+    </body>
+</html>
