a260728e7bc2db87c3309536b4280187af16a6ce
Heshan Hansana <65733812+heshanhansana@users.noreply.github.com>
2024-10-28 08:48:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    // database connection
     include('database.php');
 
 ?>
@@ -7,6 +7,14 @@
 
 
 <html>
+
+    <head>
+        <title>Let's Chat</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+
+
     <body>
 
         <div class="container" align="center">
@@ -27,9 +35,16 @@
             <div class="user_input">
 
                 <form action="" method="post">
-                    <lable>New message : </lable>
-                    <input type="text" name="msg">
-                    <input type="submit" value="Send">
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">New message : </label>
+                        <div class="col-sm-7">
+                            <input type="password" class="form-control" id="inputPassword" placeholder="Message">
+                        </div>
+
+                        <div class="col-sm-3">
+                            <input type="submit" value="Send" class="btn btn-success">
+                        </div>
+                    </div>
                 </form>
 
             </div>
