100359191caa626ff04031ad10988ab4c4253558
Heshan Hansana <65733812+heshanhansana@users.noreply.github.com>
2024-10-28 14:38:07 +0530

    update

--- send.php
+++ send.php
@@ -4,19 +4,23 @@
 
     $new_message = $_POST["new-message"];
 
-    $sql = "INSERT INTO messages(message) VALUES(?)";
-    $stmt = mysqli_stmt_init($conn);
-    $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
-                        
-    if($prepareStmt){
-        mysqli_stmt_bind_param($stmt, "s",  $new_message);
-        mysqli_stmt_execute($stmt);
-        echo "<div class='alert alert-success'>Sent !</div>";
-        header("Location: index.php");  // return to home page
+    if(!empty($new_message)){
+
+        $sql = "INSERT INTO messages(message) VALUES(?)";
+        $stmt = mysqli_stmt_init($conn);
+        $prepareStmt = mysqli_stmt_prepare($stmt, $sql);
+                            
+        if($prepareStmt){
+            mysqli_stmt_bind_param($stmt, "s",  $new_message);
+            mysqli_stmt_execute($stmt);
+        }
+    
+        else{
+            die("Message Fail !");
+        } 
+        
     }
 
-    else{
-        die("Message Fail !");
-    } 
-
+    header("Location: index.php");  // return to home page
+   
 ?>
