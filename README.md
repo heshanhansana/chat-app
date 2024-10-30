10d8bb7e64f7901db08a14888409cc7dc536c17a
Heshan Hansana <65733812+heshanhansana@users.noreply.github.com>
2024-10-30 09:32:13 +0530

    New update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # chat-app
-Team project to develop simple chat website
+Team project to develop simple chat website. 28th Octomber 2024
+Version 1 tested on 29th Octomber 2024 at UJP-FOT with hostal friends.
+
+Version 2 under development.
--- script.js
+++ script.js
@@ -7,8 +7,20 @@ const resetIdleTimer = () => {
     }, 3000); // 3 seconds of inactivity
 };
 
+
 document.addEventListener('mousemove', resetIdleTimer);
 document.addEventListener('keydown', resetIdleTimer);
 document.addEventListener('click', resetIdleTimer);
 
 resetIdleTimer(); // Start the timer immediately
+
+
+// forcus on text input field
+window.onload = function() {
+    document.getElementById("myInput").focus();
+};
+
+
+setInterval(function() {
+    $('#history').load('previous_chat.php #history');
+  }, 1500); //  1.5 seconds
