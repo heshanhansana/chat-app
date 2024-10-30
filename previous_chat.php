7d09c50eddb59daa8314bcb3fd8c01a41222c800
Heshan Hansana <65733812+heshanhansana@users.noreply.github.com>
2024-10-30 07:16:37 +0530

    New Update

--- previous_chat.php
+++ previous_chat.php
@@ -1,30 +1,31 @@
 <?php
     include('database.php');
     
-    $sql ="SELECT * FROM messages";
+    $sql = "SELECT * FROM messages ORDER BY id DESC LIMIT 10";
     $result = mysqli_query($conn, $sql);
-
-    if($result){
-        if(mysqli_num_rows($result) > 0){       ?>
-
-
-        <table class="table table-hover">
-            <tbody><?php
-            
-            while($row = mysqli_fetch_array($result)){     ?>
-
-                <tr>
-                    <td><?php echo $row['message']; ?></td>
-                    <td><?php echo "<a href='delete.php?id=" . $row["id"] . "'><input type='button' value='X' class='btn btn-outline-dark'> </a>"?></td> 
-                </tr><?php
-                }    ?>
-
-            </tbody></table>
-            
-        <?php
-
-        }else{
+    
+    if ($result) {
+        if (mysqli_num_rows($result) > 0) {
+            $messages = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            $messages = array_reverse($messages); // Reverse the array
+    
+            ?>
+    
+            <table class="table table-hover">
+                <tbody>
+                    <?php foreach ($messages as $row): ?>
+                        <tr>
+                            <td><?php echo $row['message']; ?></td>
+                            <td><?php echo "<a href='delete.php?id=" . $row["id"] . "'><input type='button' value='X' class='btn btn-outline-dark'> </a>" ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+    
+            <?php
+        } else {
             echo "<div class='alert alert-danger'>No Messages</div>";
         }
     }
+    
 ?>
